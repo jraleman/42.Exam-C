0002299d63aac4faf0c02085426f0f4c03b20102
José Ramón Alemán <jraleman@users.noreply.github.com>
2018-08-07 16:09:00 -0700

    Fix repeat_alpha.c infinite while loop

--- level01/repeat_alpha.c
+++ level01/repeat_alpha.c
@@ -25,10 +25,12 @@ int		main(int argc, char *argv[])
 			letter = 0;
 			if (argv[1][i] >= 'A' && 'Z' >= argv[1][i])
 				letter = argv[1][i] - 63;
-			if (argv[1][i] >= 'a' && 'z' >= argv[1][i])
+			else if (argv[1][i] >= 'a' && 'z' >= argv[1][i])
 				letter = argv[1][i] - 95;
-				while (--letter)
-					write(1, &argv[1][i], 1);
+          	else
+				write(1, &argv[1][i], 1);
+			while (--letter)
+				write(1, &argv[1][i], 1);
 			i += 1;
 		}
 	}
